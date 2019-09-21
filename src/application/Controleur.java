a1f9c4bae7d91f80a2541ce39d1ba89dc6d118a9
Geoffroy Muller <johndoe@example.comgeoffroym2@gmail.com>
2019-09-21 17:44:40 +0200

    initialisation

---
+++ src/application/Controleur.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controleur {
+
+    @FXML
+    public void meth() {
+        System.out.println("dddddddddddd");
+    }
+
+}
